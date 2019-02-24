f05baef1f9e44f141231ea56f34f5ec809e15eba
Fernando Wittmann <fernando.wittmann@gmail.com>
2019-02-24 10:48:15 -0300

    Add files

---
+++ cross_imdb_hbogo.py
@@ -0,0 +1,38 @@
+import json, re
+import requests as r
+import pandas as pd
+
+# Input 
+API_KEY = "84528a1f"
+OMDB_REQUEST = "http://www.omdbapi.com/?apikey={}&t={}"
+
+with open('hbo_element.html', 'r') as myfile:
+    data=myfile.read()
+
+hbo_movie_names = re.findall( r'itemName">(.*?)</span>', data)
+
+#hbo_movie_names = hbo_movie_names[:10]
+
+imdb_ratings = {}
+
+for title in hbo_movie_names:
+    try:
+        movie = r.get(OMDB_REQUEST.format(API_KEY, title.replace(' ', '+'))).content
+        print(movie)
+        movie = json.loads(movie)
+        rating = float(movie["Ratings"][0]["Value"].replace("/10",""))
+        print(rating)
+        imdb_ratings[title] = rating
+
+    except Exception as e:
+        print("##### ERROR #####")
+        print(e)
+
+print(imdb_ratings)
+
+df = pd.Series(imdb_ratings)
+df = df.sort_values(ascending=False)
+
+print(df)
+
+df.to_csv('results.csv')
